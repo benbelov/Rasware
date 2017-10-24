0be48c25e2d23cb3882e6466c7913a6e0c523815
thetianshuhuang <thetianshuhuang@gmail.com>
2017-10-24 15:02:59 -0500

    Added function GhettoPrintf for handling long data types

--- Pulse/Main.c
+++ Pulse/Main.c
@@ -9,10 +9,15 @@
 #include <StellarisWare/driverlib/systick.h>
 
 tBoolean led_on;
-tTime timeout_us = 1000;
+tTime timeout_us = 5000;
+
+void GhettoPrintf(char *message,tTime number){
+  Printf("%s: %d.%d\n", message, (int) ((long)number / 10000), (int) ((long)number % 10000));
+}
 
 void pulsePin(void){
 
+  
   SetPin(PIN_F1, led_on);
   SetPin(PIN_F3, !led_on);
 
@@ -20,9 +25,9 @@ void pulsePin(void){
 
   unsigned long ulSysPeriod = ROM_SysCtlClockGet();
 
-  int divfive = (int) ulSysPeriod / 1000;
+  int divfive = (int) (ulSysPeriod / 1000);
 
-  int delay = (int) ulSysPeriod*0.000005 - 80;
+  int delay = (int) (ulSysPeriod*0.000005 - 80);
 
   
   tTime start_time_1 = 0;
@@ -31,15 +36,18 @@ void pulsePin(void){
   tTime end_time_2 = 0;
   tTime timeout_time = GetTimeUS() + timeout_us;
 
+  GhettoPrintf("Timeout time",timeout_time);
+  GhettoPrintf("Start time",GetTimeUS());
+  
   SetPin(PIN_B1, true);
   SetPin(PIN_D1, true);
   ROM_SysCtlDelay(delay);
   SetPin(PIN_B1, false);
   SetPin(PIN_D1, false);
 
-  while ( GetTimeUS() < timeout_time && (end_time_1 == 0 || end_time_2 == 0)) {
-    Printf("Time: %d", (int) GetTimeUS() % 10000);
-    if (GetPin(PIN_B2) == true && start_time_1 == 0) {
+  while ( GetTimeUS() < timeout_time) {
+    GhettoPrintf("Current time", GetTimeUS());
+    /*if (GetPin(PIN_B2) == true && start_time_1 == 0) {
       start_time_1 = GetTimeUS();
     }
     if (GetPin(PIN_B3) == true && start_time_2 == 0) {
@@ -51,6 +59,7 @@ void pulsePin(void){
     if (GetPin(PIN_B3) == false && start_time_2 != 0) {
       end_time_2 = GetTimeUS();
     }
+    */
   }
 
   int time_1 = (int) end_time_1 - start_time_1;
@@ -59,12 +68,13 @@ void pulsePin(void){
   Printf("Time 1: %d us\n", time_1);
   Printf("Time 2: %d us\n", time_2);
   Printf("Clock : %d kHz\n", divfive);
+  Printf("\n");
   
 }
 
 int main(void){
-
-  Printf("hello world");
+  
+  Printf("hello world\n");
   CallEvery(pulsePin, 0, 1);
 
   return 0;
