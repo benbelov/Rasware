9781ace502bbffaf48c26fc71dc61192bde47791
thetianshuhuang <thetianshuhuang@gmail.com>
2017-10-24 16:51:42 -0500

    More workarounds! This time, time travel detection of GetTimeUS(). Also added comments and memes.

--- Pulse/Main.c
+++ Pulse/Main.c
@@ -9,45 +9,67 @@
 #include <StellarisWare/driverlib/systick.h>
 
 tBoolean led_on;
-tTime timeout_us = 5000;
+// How long should we wait for the distance sensors?
+tTime timeout_us = 10000;
 
+// Printf from uart.h doesn't support long. Here we go!
+// The number is cast to long since the fucking launchpad doesn't seem to support the division of long long int; (long long int) x / n or x % n gives make errors, but +,-,* are fine.
 void GhettoPrintf(char *message,tTime number){
   Printf("%s: %d.%d\n", message, (int) ((long)number / 10000), (int) ((long)number % 10000));
 }
 
 void pulsePin(void){
 
-  
+  // Heartbeat LED
   SetPin(PIN_F1, led_on);
   SetPin(PIN_F3, !led_on);
-
   led_on = !led_on;
 
+  // Computation of clock cycles necessary for 15us(+/-20ns) pulse
+  // 80 clocks is the approximate execution time of 2x SetPin
+  // ROM_SysCtlClockGet is much more consistent than SysCtlClockGet
   unsigned long ulSysPeriod = ROM_SysCtlClockGet();
-
-  int divfive = (int) (ulSysPeriod / 1000);
-
   int delay = (int) (ulSysPeriod*0.000005 - 80);
 
-  
+  // Initialize variables for the loop
   tTime start_time_1 = 0;
   tTime start_time_2 = 0;
   tTime end_time_1 = 0;
   tTime end_time_2 = 0;
-  tTime timeout_time = GetTimeUS() + timeout_us;
-
-  GhettoPrintf("Timeout time",timeout_time);
-  GhettoPrintf("Start time",GetTimeUS());
+  tTime start_time = GetTimeUS();
+  tTime timeout_time = start_time + timeout_us;
+  long time_remaining_previous = timeout_time;
   
+  // Pulse pins by 15us
   SetPin(PIN_B1, true);
   SetPin(PIN_D1, true);
   ROM_SysCtlDelay(delay);
   SetPin(PIN_B1, false);
   SetPin(PIN_D1, false);
 
-  while ( GetTimeUS() < timeout_time) {
-    GhettoPrintf("Current time", GetTimeUS());
-    /*if (GetPin(PIN_B2) == true && start_time_1 == 0) {
+  // Sensor loop
+  // while (true) is used with break to minimize the chance of launchpad fuckery
+  while ( true) {
+
+    // Compute Time
+    long current_time = (long) GetTimeUS();
+    long time_remaining = timeout_time - current_time;
+
+    // Defense against time travelling GetTimeUS()
+    // Generally speaking, GetTimeUS() will time travel twice on boot
+    if( time_remaining_previous < time_remaining) {
+      GhettoPrintf("Time fault",time_remaining);
+      break;
+    }
+    time_remaining_previous = time_remaining;
+
+    // End condition
+    if( time_remaining <= 0) {
+      break;
+    }
+
+    // Record the pulse start and end times.
+    if (GetPin(PIN_B2) == true && start_time_1 == 0) {
       start_time_1 = GetTimeUS();
     }
     if (GetPin(PIN_B3) == true && start_time_2 == 0) {
@@ -59,22 +81,31 @@ void pulsePin(void){
     if (GetPin(PIN_B3) == false && start_time_2 != 0) {
       end_time_2 = GetTimeUS();
     }
-    */
+   
   }
 
+  // Compute pulse width
+  // (int) is used since max pulse length is ~22000us
   int time_1 = (int) end_time_1 - start_time_1;
   int time_2 = (int) end_time_2 - start_time_2;
-   
+
+  // Compute distance
+  // t = 2d/v => d = tv/2 = 34000*t(us)/(1000000*2) = 0.017t(us) cm
+  float distance_1 = (float) time_1 * 0.017000;
+  float distance_2 = (float) time_2 * 0.017000;
+
+  // Print out info
   Printf("Time 1: %d us\n", time_1);
   Printf("Time 2: %d us\n", time_2);
-  Printf("Clock : %d kHz\n", divfive);
+  Printf("Distance 1: %f cm\n", distance_1);
+  Printf("Distance 2: %f cm\n", distance_2);
   Printf("\n");
-  
+
 }
 
 int main(void){
   
-  Printf("hello world\n");
+  Printf("Hello World\n\n");
   CallEvery(pulsePin, 0, 1);
 
   return 0;
