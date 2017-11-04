e577d8093cebe9e407170abc3cc19d20109be302
thetianshuhuang <thetianshuhuang@gmail.com>
2017-11-04 11:22:24 -0500

    Added isObtuse as an example for how math.c and nav.c functions should be implemented.

--- Blackbird/main.h
+++ Blackbird/main.h
@@ -16,17 +16,17 @@ void LoopDelay(float time);
 void PulsePin(tPin pin);
 float * calculateDistance(long start_time_1, long end_time_1, long start_time_2, long end_time_2);
 float * getDistance(long timeout_us,tPin * trigger_pins,tPin echo_1, tPin echo_2);
-int checkRamp();
+int checkRamp(); // not done
 
 // math.c
-float * cartesian(pointSet points);
-int isObtuse(float a_x,float a_y,float b_x,float b_y,float c_x,float c_y);
-float inverseTangent(float ratio);
+float * cartesian(pointSet points); // not done
+int isObtuse(float a_x,float a_y,float b_x,float b_y,float c_x,float c_y); // not done
+float inverseTangent(float ratio); // not done
 
 // nav.c
-int * indexObstacles(pointSet points);
-float chooseTarget(pointSet points);
-float pidControl(float target, float * targetHistory);
+int * indexObstacles(pointSet points); // not done
+float chooseTarget(pointSet points); // not done
+float pidControl(float target, float * targetHistory); // not done
 
 // control.c
-void setMotors(float pidCoefficient,float sensitivity);
+void setMotors(float pidCoefficient,float sensitivity); // not done
--- Blackbird/math.c
+++ Blackbird/math.c
@@ -1,2 +1,21 @@
 // math.c in /Blackbird
 // Math routines for the navigation algorithm
+
+#include <math.h>
+
+// checks if an angle <ABC between three cartesian coordinates is obtuse
+// returns 1 if obtuse, 0 if not obtuse
+int isObtuse(float a_x,float a_y,float b_x,float b_y,float c_x,float c_y) {
+
+  // using the pythagorean theorem:
+  float index_1 = pow(a_x-b_x,2) + pow(a_y-b_y,2)
+                + pow(b_x-c_x,2) + pow(b_y-c_y,2);
+  float index_2 = pow(c_x-c_a,2) + pow(c_y-a_y,2);
+
+  if(index_1 > index_2) {
+    return(0);
+  }
+  else {
+    return(1);
+  }
+}
