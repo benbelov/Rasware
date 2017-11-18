878a8e8868f4a517f12700f70aa722ea466a037c
benbelov <bbelov@gmail.com>
2017-11-17 20:27:19 -0600

    added neg handeling to linearizeMotors

--- Blackbird/control.c
+++ Blackbird/control.c
@@ -16,58 +16,46 @@ void setMotors(float pidCoefficient,HWProfile * profile) {
 }
 
 // Linearize motor profile
+/*
+*
+*  y -function output   x- pidCoefficient arguement
+*	0.05			0.109375
+*	0.10			0.359375
+*	0.15			0.53125
+*	0.20			0.71875
+*	0.30			0.9375
+*	0.40			1
+*
+*/
+/* (yhigh - ylow)/(xhigh - xlow) = slope between two adjacent points. 
+ * y = ylow + slope(pidCoefficient - xlow), where ylow is the y value below the pidCoefficient arguement, 
+ * and yhigh is the y value above it.
+ */
 float linearizeMotors(float pidCoefficient) {
 
-/*
+	float yValues[] = {0, 0.05, 0.1, 0.15, 0.2, 0.3, 0.4};
+	float xValues[] = {0, 0.109375, 0.359375, 0.53125, 0.71875, 0.9375, 1};
 
-  y -function output   x- pidCoefficient arguement
-	0.05			0.109375
-	0.10			0.359375
-	0.15			0.53125
-	0.20			0.71875
-	0.30			0.9375
-	0.40			1
-
-*/
-
-// (yhigh - ylow)/(xhigh - xlow) = slope between two adjacent points. y = ylow + slope(pidCoefficient - xlow), where ylow is the y value below the pidCoefficient arguement, and yhigh is the y value above it.
+	int neg = 1;
 
 	if (pidCoefficient > 1){
 
 		return 1;
 
 	}
-	else if (pidCoefficient < 0){
-
-		return 0;
+	if (pidCoefficient < 0 && pidCoefficient >= -1){
+		neg = -1;
+		pidCoefficient = -pidCoefficient;
 	}
-	else{
 
-		float yValues[] = {0, 0.05, 0.1, 0.15, 0.2, 0.3, 0.4};
-		float xValues[] = {0, 0.109375, 0.359375, 0.53125, 0.71875, 0.9375, 1};
+	for(int index = 1; i < 7; i++){
 
-		int index = 1;
-
-		while (index < 7){
-
-			if (pidCoefficient <= xValues[index]){
-			
-				return yValues[index-1] + 
-				(((yValues[index] - yValues[index-1])/(xValues[index] - xValues[index-1]))*
-				(pidCoefficient - xValues[index-1]));
-
-				index = 8; //Break out of while loop instead of using break;
-
-			}else{
-
-				index += 1;
-
-			}
-
-
+		if (pidCoefficient <= xValues[index]){
+			return neg * (yValues[index-1] + 
+			(((yValues[index] - yValues[index-1])/(xValues[index] - xValues[index-1]))*
+			(pidCoefficient - xValues[index-1])));
 		}
-
-	}
-	return 0;
+	}		
+	return -1;
 
 }
